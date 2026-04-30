6508c46f6d40d430e3e4194dd6cde5231baa2cf1
coldwater78910 <coldwater78910@gmail.com>
2026-04-30 17:49:20 +0200

    Rename photos to simple numbered names (photo1.jpg, photo2.jpg, etc.)

---
+++ photos/rename_photos.sh
@@ -0,0 +1,29 @@
+mv "WhatsApp Image 2026-04-30 at 17.39.44.jpeg" photo1.jpg
+mv "WhatsApp Image 2026-04-30 at 17.39.45 (1).jpeg" photo2.jpg
+mv "WhatsApp Image 2026-04-30 at 17.39.45.jpeg" photo3.jpg
+mv "WhatsApp Image 2026-04-30 at 17.39.46 (1).jpeg" photo4.jpg
+mv "WhatsApp Image 2026-04-30 at 17.39.46.jpeg" photo5.jpg
+mv "WhatsApp Image 2026-04-30 at 17.44.17 (1).jpeg" photo6.jpg
+mv "WhatsApp Image 2026-04-30 at 17.44.17.jpeg" photo7.jpg
+mv "WhatsApp Image 2026-04-30 at 17.44.18 (1).jpeg" photo8.jpg
+mv "WhatsApp Image 2026-04-30 at 17.44.18 (2).jpeg" photo9.jpg
+mv "WhatsApp Image 2026-04-30 at 17.44.18.jpeg" photo10.jpg
+mv "WhatsApp Image 2026-04-30 at 17.44.19 (1).jpeg" photo11.jpg
+mv "WhatsApp Image 2026-04-30 at 17.44.19.jpeg" photo12.jpg
+mv "WhatsApp Image 2026-04-30 at 17.44.20.jpeg" photo13.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.45.jpeg" photo14.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.46 (1).jpeg" photo15.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.46 (2).jpeg" photo16.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.46.jpeg" photo17.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.47 (1).jpeg" photo18.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.47 (2).jpeg" photo19.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.47.jpeg" photo20.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.48 (1).jpeg" photo21.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.48 (2).jpeg" photo22.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.48 (3).jpeg" photo23.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.48.jpeg" photo24.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.49.jpeg" photo25.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.50 (1).jpeg" photo26.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.50 (2).jpeg" photo27.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.50.jpeg" photo28.jpg
+mv "WhatsApp Image 2026-04-30 at 17.45.51.jpeg" photo29.jpg
